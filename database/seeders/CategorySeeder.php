db2a79f00033a6f0b963e1999d300716ecf9903e
himalsimkhada <himalsimkhada88@gmail.com>
2021-09-28 15:46:28 +0545

    .

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin\Category;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -13,6 +14,69 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'जीवनशैली',
+            'slug' => 'lifestyle',
+            'active' => 1,
+            'position' => 3,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'अन्तर्राष्ट्रिय',
+            'slug' => 'international',
+            'active' => 1,
+            'position' => 2,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'खेलकुद',
+            'slug' => 'sports',
+            'active' => 1,
+            'position' => 6,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'सूचना प्रविधि',
+            'slug' => 'communications',
+            'active' => 1,
+            'position' => 4,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'मनोरन्जन',
+            'slug' => 'entertainment',
+            'active' => 1,
+            'position' => 5,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'अर्थ',
+            'slug' => 'finance',
+            'active' => 1,
+            'position' => 7,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'अन्य',
+            'slug' => 'others',
+            'active' => 1,
+            'position' => 8,
+            'description' => 'Test',
+        ]);
+        Category::create([
+            'code' => rand(4000, 9000),
+            'name' => 'प्रदेश',
+            'slug' => 'state',
+            'active' => 1,
+            'position' => 1,
+            'description' => 'Test',
+        ]);
     }
 }
