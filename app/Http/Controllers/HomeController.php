37c54ad7e2ef23c48c01749b807802909656fa98
himalsimkhada <himalsimkhada88@gmail.com>
2021-09-30 14:27:07 +0545

    .'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -53,18 +53,19 @@ class HomeController extends Controller {
      */
     public function show($slug) {
 
-        $post = Post::all();
+        // $posts = Post::all();
 
         $post = Post::with('author', 'tags')->where('slug', $slug)->first();
+        // dd($post);
         $tagId = [];
         foreach ($post->tags as $tag) {
             $tagId[] = $tag['id'];
         }
         $tag = Tag::whereIn('id', $tagId)->get();
         $postId = [];
-        foreach ($tag as $tag) {
-            foreach ($tag->posts as $post) {
-                $postId[] = $post->id;
+        foreach ($tag as $value) {
+            foreach ($value->posts as $value2) {
+                $postId[] = $value2->id;
             }
         }
         sort($postId);
@@ -72,6 +73,7 @@ class HomeController extends Controller {
         $relatedPost = Post::whereIn('id', $postId)->limit(8)->get();
         // dd($relatedPost);
         $nepaliDate = NepaliDate::create($post->created_at)->toFormattedNepaliDate();
+        // dd($post);
         return view('nepali.details', compact('post', 'nepaliDate', 'relatedPost'));
     }
 
