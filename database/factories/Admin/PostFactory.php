d661be0d35c15c005db221867f9d4fcee55b25fa
himalsimkhada <himalsimkhada88@gmail.com>
2021-10-01 12:32:10 +0545

    .

--- database/factories/Admin/PostFactory.php
+++ database/factories/Admin/PostFactory.php
@@ -20,15 +20,23 @@ class PostFactory extends Factory {
      * @return array
      */
     public function definition() {
-        $title = $this->faker->sentence();
+        $title = [
+            'विराटनगरमाथि ललितपुरको ईपीएलकै ठूलो स्कोर प्रहार',
+            'एमाले विधान महाधिवेशन : प्रतिनिधि गोदावरी पुगे, हल प्रवेश गर्ने क्रम सुरु',
+            'बर्सेनि अर्बौं लगानी तर कुपोषण समस्या उस्तै',
+            'भट्टराईको प्रश्न : द्वन्द्वका घाउ कोट्याएर क्यान्सर बनाउन खोजेको हो ?',
+            'जडिबुटी संकलनले मुगुमा जनगणना प्रभावित',
+            'एमसीसीमा विवाद गर्नु दुर्भाग्यपूर्ण : भट्टराई',
+        ];
+        $random_title = array_rand($title);
         return [
             'code' => rand(100000, 999999),
-            'title' => $title,
+            'title' => $random_title,
             'code' => rand(100, 3000000),
-            'slug' => Str::slug($title),
+            'slug' => Str::slug($this->faker->sentence),
             'category_id' => rand(1, 8),
             'author_id' => 1,
-            'body' => '<p>' . $this->faker->paragraph(3, false)   . '</p>',
+            'body' => '<p>' . 'जनता समाजवादी पार्टी नेपालका संघीय परिषद् अध्यक्ष बाबुराम भट्टराईले राष्ट्रिय सहमति जुटाएर एमसीसी पास गर्नुपर्ने बताएका छन् । जनता समाजवादीले सिन्धुलीमाढीमा मंगलबार आयोजना गरेको कार्यक्रममा भट्टराईले एमसीसी सहयोगको विरोध हुनु दुर्भाग्यपूर्ण भएको बताए ।'   . '</p>',
             'image' => 'news/post/default/adoBPqzQxJ4tkqosamu8L6ShhQ21Ag9jYnNfRUtN.jpg',
             'featured' => rand(0, 1),
             'status' => rand(0, 3),
