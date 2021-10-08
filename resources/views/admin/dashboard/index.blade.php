9132f5e5e2d031f1138adf8020a8e81891bbc7ac
himalsimkhada <himalsimkhada88@gmail.com>
2021-10-08 13:24:58 +0545

    .

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -37,7 +37,7 @@
             </div>
             <div class="col-sm-6 col-xl-3 col-lg-6">
                 <div class="card o-hidden">
-                    <div class="bg-secondary b-r-4 card-body">
+                    <div class="bg-primary b-r-4 card-body">
                         <div class="media static-top-widget">
                             <div class="align-self-center text-center"><i data-feather="calendar"></i></div>
                             <div class="media-body"><span class="m-0">Weekly Views</span>
